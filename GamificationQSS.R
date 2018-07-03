9e7f900b13b3e3565b34c69c50b78546f8698414
DasHammett <clockwork80@gmail.com>
2018-07-03 10:24:12 +0200

    Added UTF-16 file selection to converter scripts. Added comments to each step

--- GamificationQSS.R
+++ GamificationQSS.R
@@ -1,6 +1,12 @@
-setwd("/Users/jvidal/Desktop/ R scripts/")
-system("iconv -f UTF-16LE -t UTF-8 ~/data.csv > raw_data.csv") # Convert UTF-16LE encoding to UTF-8 using bash
-Raw.data <- read.csv("raw_data.csv",header = T, stringsAsFactors = F, sep = "\t", dec = ".")
+library(dplyr)
+setwd("/Users/jvidal/Desktop/ R scripts/") # Set working directory
+
+file <- file.path(file.choose()) # Gather UTF-16LE file path. File should contain no spaces.
+system(paste("iconv -f UTF-16LE -t UTF-8",file,"> raw_data.csv", sep = " ")) # Convert UTF-16LE encoding to UTF-8 using bash
+rm(file) # Remove object file as it is no longer needed
+
+Raw.data <- read.csv("raw_data.csv",header = T, stringsAsFactors = F, sep = "\t", dec = ".") # Load file into object Raw.data
+
 Raw.data <- Raw.data %>%
   mutate_all(funs(gsub("%","",.))) %>% # Remove % sign
   mutate_at(vars(case_id,Attr..,Adoption..Self.,Adoption..IQE.),as.numeric) %>% # Transform columns of intereset to numeric
@@ -10,12 +16,12 @@ Raw.data <- Raw.data %>%
 Gamification <- list() # Create empty list to avoid cluttering the Global Environment. Next objects will be stored inside list
 
 ## Knowledge
-Gamification$K <- Raw.data %>%
-  filter(Attribute..copy. == "Knowledge") %>%
-  distinct(case_id, .keep_all = T) %>%
-  group_by(L1.Mgr) %>%
-  summarise(N = n(),
-            Knowledge = mean(Attr.., na.rm = T))
+Gamification$K <- Raw.data %>% # Store output in Gamification list with name "K"
+  filter(Attribute..copy. == "Knowledge") %>% # Filter Attribute to Knowledge
+  distinct(case_id, .keep_all = T) %>% # Remove duplicated case_id
+  group_by(L1.Mgr) %>% # Do calculations per TL
+  summarise(N = n(), # Count number of evaluations
+            Knowledge = mean(Attr.., na.rm = T)) # Calculate average of Knowledge
 
 ## Compliance
 Gamification$C <- Raw.data %>%
@@ -27,16 +33,16 @@ Gamification$C <- Raw.data %>%
 
 ## Adoption 
 Gamification$A <- Raw.data %>%
-  mutate(Adoption = ifelse(is.na(Adoption..Self.), Adoption..IQE.,Adoption..Self.)) %>% 
+  mutate(Adoption = ifelse(is.na(Adoption..Self.), Adoption..IQE.,Adoption..Self.)) %>% # Create new column Adoption with the scores from self and IQE evaluations
   distinct(case_id, .keep_all = T) %>%
   group_by(L1.Mgr) %>%
   summarise(N = n(),
             Adoption = mean(Adoption, na.rm = T))
 
 ## Merge all together
-Gamification$QSS <- left_join(Gamification$K,Gamification$C, by = c("L1.Mgr","N")) %>%
-  left_join(.,Gamification$A, by = c("L1.Mgr","N")) %>%
-  mutate(QSS = Knowledge * 0.25 + Adoption * 0.25 + Compliance * 0.5)
+Gamification$QSS <- left_join(Gamification$K,Gamification$C, by = c("L1.Mgr","N")) %>% # Join Knowledge and Compliance objects by TL and Number of evaluations (to avoid multiple columns)
+  left_join(.,Gamification$A, by = c("L1.Mgr","N")) %>% # Join previous step with Adoption
+  mutate(QSS = Knowledge * 0.25 + Adoption * 0.25 + Compliance * 0.5) # Create QSS metric with the weights
 
 ## Write output file
 write.csv2(Gamification$QSS,"Gamification_QSS.csv",row.names = F)
